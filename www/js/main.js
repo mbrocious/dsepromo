c36d33a7dd74c427d912b2de12593e8a09d9ab5d
Mike Brocious <mike@brocious.net>
2012-11-18 19:39:23 -0500

    first commit

---
+++ www/js/main.js
@@ -0,0 +1,18 @@
+
+
+$(function() {
+//	$('.button.dsepromo').click(function() {
+		// window.location.assign('http://diversifiedsourcingent.espwebsite.com/');
+//		$.mobile.loading( 'show' , {
+//			text: 'Going to the mall!',
+//			textVisible: true,
+//			theme: 'e'
+//		});
+//	});
+
+//	$(window).unload(function() {
+//		$.mobile.loading( 'hide' );
+//	});
+});
+
+
